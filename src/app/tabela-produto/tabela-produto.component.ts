874afc1d9c7cdd95e448247e6345db0d82c04678
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-04-10 20:04:40 -0300

    feat: adiciona página html para lista de produtos e chama método de listar produtos

--- src/app/tabela-produto/tabela-produto.component.ts
+++ src/app/tabela-produto/tabela-produto.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Produto, ProdutoService } from '../produto.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabela-produto',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tabela-produto.component.html',
   styleUrl: './tabela-produto.component.css'
 })
 export class TabelaProdutoComponent {
+  produtos: Produto[] = [];
+
+  constructor(private produtoService: ProdutoService) { }
+
+  ngOnInit() {
+    this.produtos = this.produtoService.listarProdutos();
+  }
 
 }
