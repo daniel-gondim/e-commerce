4b6adbcf52e9c13474c3b37cf8ef0d3cfc10b5da
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-04-10 20:01:46 -0300

    feat: adiciona página html para o cadastro de produtos e o método adicionarProduto()

--- src/app/cadastro-produto/cadastro-produto.component.ts
+++ src/app/cadastro-produto/cadastro-produto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProdutoService } from '../produto.service';
 
 @Component({
   selector: 'app-cadastro-produto',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './cadastro-produto.component.css'
 })
 export class CadastroProdutoComponent {
+  id = 0;
+  nome = '';
+  preco = 0;
+
+  constructor(private produtoService: ProdutoService) { }
+
+  adicionarProduto() {
+    this.produtoService.adicionarProduto({ id: this.id, nome: this.nome, preco: this.preco });
+    this.id =  0;
+    this.nome = '';
+    this.preco = 0;
+  }
 
 }
