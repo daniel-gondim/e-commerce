381f67f8705e088ee34d67ebb23dbf5bf35ffbc1
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-04-10 22:00:58 -0300

    test: altera nomes dos métodos de teste

--- src/app/produto.service.spec.ts
+++ src/app/produto.service.spec.ts
@@ -1,46 +1,46 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { ProdutoService } from './produto.service';
-import { Produto } from './produto.service'; 
+import { Produto } from './produto.service';
 
 describe('ProdutoService', () => {
- let service: ProdutoService;
- let httpMock: HttpTestingController;
+  let service: ProdutoService;
+  let httpMock: HttpTestingController;
 
- beforeEach(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule], // Importa o HttpClientTestingModule para simular requisições HTTP
-      providers: [ProdutoService]
+      imports: [HttpClientTestingModule],
+      providers: [ProdutoService],
     });
     service = TestBed.inject(ProdutoService);
-    httpMock = TestBed.inject(HttpTestingController); // Injeta o HttpTestingController para interceptar e simular respostas HTTP
- });
+    httpMock = TestBed.inject(HttpTestingController);
+  });
 
- afterEach(() => {
-    httpMock.verify(); // Verifica se todas as requisições esperadas foram feitas
- });
+  afterEach(() => {
+    httpMock.verify();
+  });
 
- it('should be created', () => {
+  it('deve criar o serviço ProdutoService', () => {
     expect(service).toBeTruthy();
- });
+  });
 
- it('should list products', () => {
+  it('deve listar todos os produtos', () => {
     const dummyProdutos: Produto[] = [
       { id: 1, nome: 'Produto 1', preco: 100 },
-      { id: 2, nome: 'Produto 2', preco: 200 }
+      { id: 2, nome: 'Produto 2', preco: 200 },
     ];
 
-    service.listarProdutos().subscribe(produtos => {
+    service.listarProdutos().subscribe((produtos) => {
       expect(produtos.length).toBe(2);
       expect(produtos).toEqual(dummyProdutos);
     });
 
-    const req = httpMock.expectOne('http://localhost:3000/produtos'); // Espera uma requisição para a URL especificada
+    const req = httpMock.expectOne('http://localhost:3000/produtos');
     expect(req.request.method).toBe('GET');
-    req.flush(dummyProdutos); // Simula uma resposta com os produtos dummy
- });
-
- // Aqui você pode adicionar testes para os outros métodos do serviço
+    req.flush(dummyProdutos);
+  });
 });
-
