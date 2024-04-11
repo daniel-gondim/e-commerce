5142de32e9cbb0d5909a248ac1a79f28a2aae4e9
Daniel Gondim <danielgondimdecastro@gmail.com>
2024-04-10 21:52:08 -0300

    feat: importa FormsModule para o componente

--- src/app/cadastro-produto/cadastro-produto.component.ts
+++ src/app/cadastro-produto/cadastro-produto.component.ts
@@ -1,25 +1,29 @@
 import { Component } from '@angular/core';
 import { ProdutoService } from '../produto.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-produto',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cadastro-produto.component.html',
-  styleUrl: './cadastro-produto.component.css'
+  styleUrl: './cadastro-produto.component.css',
 })
 export class CadastroProdutoComponent {
   id = 0;
   nome = '';
   preco = 0;
 
-  constructor(private produtoService: ProdutoService) { }
+  constructor(private produtoService: ProdutoService) {}
 
   adicionarProduto() {
-    this.produtoService.adicionarProduto({ id: this.id, nome: this.nome, preco: this.preco });
-    this.id =  0;
+    this.produtoService.adicionarProduto({
+      id: this.id,
+      nome: this.nome,
+      preco: this.preco,
+    });
+    this.id = 0;
     this.nome = '';
     this.preco = 0;
   }
-
 }
